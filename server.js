8a2fc33c4395a936b7461068ff8adee18ed2ac92
Arbaaz <arbaazahussain786@gmail.com>
2024-02-14 10:44:54 +0000

    Working on Find database

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -10,43 +10,55 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 
-app.get(`/M00872279`, (request, response) => {
-  // Get data from MongoDB
+//set up database
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1, strict: false}});
+const database = client.db("FocusON-X");
+const usersCollection = database.collection("users");
+const postsCollection = database.collection("posts");
 
-  response.send({message: "hello"});
+
+app.get(`/M00872279`, async (request, response) => {
+  // Get data from MongoDB
+    const data = await postsCollection.find({}).toArray();
+    console.log(data);
+
+
+//   const collection =database.collection("posts");
+//   const collection =database.collection("users");
+
+  response.send(data);
 });
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb://localhost:27017";
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
 
-    const client = new MongoClient(uri);
 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
 
-        // Make the appropriate DB calls
-        await listDatabases(client);
+//         // Make the appropriate DB calls
+//         await listDatabases(client);
 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
 
-async function listDatabases(client){
-    const databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client){
+//     const databasesList = await client.db().admin().listDatabases();
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
