b19f2425eb43290de90b202dd47e5867f59a989b
Arbaaz <arbaazahussain786@gmail.com>
2024-02-14 09:37:37 +0000

    Fixing path & server.js

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 import { MongoClient } from 'mongodb';
 import express from 'express';
-import path from 'path';
 import bodyParser from 'body-parser';
-import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
-const studentid = 'M00872279';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Serve up static files from the public folder
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static('./public'));
 
-// Middleware to set the correct MIME type for CSS files
-app.use((req, res, next) => {
-  if (req.url.endsWith('.css')) {
-    res.setHeader('Content-Type', 'text/css');
-  }
-  next();
-});
 
-// Serve up static files from the public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get(`/${studentid}`, (request, response) => {
+app.get(`/M00872279`, (request, response) => {
   // Get data from MongoDB
-  const filePath = path.join(__dirname, '/public/index.html');
-  response.sendFile(filePath);
+
+  response.send({message: "hello"});
 });
 
 async function main(){
@@ -58,12 +42,12 @@ async function main(){
 main().catch(console.error);
 
 async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
+    const databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}/${studentid}`);
+  console.log(`Example app listening on port ${port}`);
 });
