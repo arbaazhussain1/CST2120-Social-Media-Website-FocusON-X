844849827ae3b219540e4c18c0038d1d0c93c71d
Arbaaz <arbaazahussain786@gmail.com>
2024-02-27 19:10:40 +0000

    Create modal for the main content posts and display both images & videos as well as an Like, Share, and Saved button.

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -46,3 +46,23 @@ function sidebar() {
 //       popup.style.display = 'none';
 //   });
 // });
+
+
+
+// JavaScript function to openModalContent
+function openModalContent() {
+  var myModal = new bootstrap.Modal(document.getElementById('Modal-Content'));
+  myModal.show();
+}
+
+// JavaScript function to openModalContent2
+function openModalContent2() {
+  var myModal = new bootstrap.Modal(document.getElementById('Modal-Content2'));
+  myModal.show();
+}
+
+// JavaScript function to openModalContent3
+function openModalContent3() {
+  var myModal = new bootstrap.Modal(document.getElementById('Modal-Content3'));
+  myModal.show();
+}
